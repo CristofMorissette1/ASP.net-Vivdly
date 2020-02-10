217cb643bb19b290507f9180548b07743af6e566
Cristof Morissette <CMorissette@WynfordGroup.local>
2020-02-10 14:53:49 -0700

    model views done

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -7,20 +7,32 @@ using Vivdly.Models;
 
 namespace Vivdly.Controllers
 {
+
     public class MoviesController : Controller
     {
         // GET: Movies
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1"},
+                new Customer { Name = "Customer2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                customers = customers
+            };
+
+            return View(viewModel);
         }
         
-        [Route("movies/released/{year:regex(\\d{4})}/{month}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-    }
+        //[Route("movies/released/{year:regex(\\d{4})}/{month}")]
+       //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+    } 
 }
